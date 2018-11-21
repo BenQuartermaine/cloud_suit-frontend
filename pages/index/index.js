180f8787dbdc9714186435fd1e23c2d7ca6c35e8
Ben Quartermaine <ben.quartermaine3@gmail.com>
2018-11-21 11:32:48 +0800

    added drop down passenger selection funcitonality

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,16 @@
 //获取应用实例
 
 Page({
-  data: {},
+  data: {
+   array: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  },
+  
+bindPickerChange: function (e) {
+    console.log('the chosen one!', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
   //事件处理函数
   getUserInfo: function (e) {
     console.log(e)
