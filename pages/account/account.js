961fb196d17c6d52890786aa51630f94cda1fa43
Benjamin Chen <yeehenchen@gmail.com>
2018-11-21 16:11:42 +0800

    login 50 %

--- pages/account/account.js
+++ pages/account/account.js
@@ -11,20 +11,7 @@ Page({
 },
 
   onLoad: function (options) {
-    let that = this;
 
-    wx.request({
-      url: 'https://cloud-suite.herokuapp.com/api/v1/jets',
-      method: 'GET',
-      success(res) {
-        console.log(res)
-        const jets = res.data
-        that.setData(
-          jets
-        )
-      }
-
-    })
   },
 
   bindPickerChange: function (e) {
@@ -35,6 +22,35 @@ Page({
   },
   //事件处理函数
   getUserInfo: function (e) {
+    let that = this
+    const host = 'https://cloud-suite.herokuapp.com/'
+    console.log('processing to login')
+    // get tecent code
+    wx.login({
+      success: (res) => {
+        // give code to backend
+        wx.request({
+          url: host + 'login',
+          method: 'post',
+          data: {
+            code: res.code
+          },
+          // backend return openid
+          success: (res) => {
+            console.log(res)
+            let id = { id: res.data.userId }
+            // save openid to local storage
+            wx.setStorageSync("currentUser", id)
+            that.getinfo(e)
+          }
+        })
+      }
+    })
+
+
+  },
+
+  getinfo(e) {
     // update local storage with user info
     let user = wx.getStorageSync("currentUser")
     let currentUser = Object.assign(user, e.detail.userInfo)
