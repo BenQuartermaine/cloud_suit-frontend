90069e8f76d4abff826c1345345bce7a3c554a0a
claire chen <clairewenhuang.chen@gmail.com>
2018-11-21 11:14:24 +0800

    Resolved conflicts

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,72 +1,7 @@
 // pages/index/index.js
 Page({
-<<<<<<< HEAD
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
-})
-=======
-  data: {},
+data: {},
   //事件处理函数
   getUserInfo: function (e) {
     console.log(e)
@@ -81,4 +16,4 @@ Page({
     });
   }
 })
->>>>>>> fe7f71f521ca441b0cf3e3a37d7d7b9391005466
+
