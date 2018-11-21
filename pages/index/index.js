165705d5ebd264a1a85a4e5d8a57884690d0eaa0
Benjamin Chen <yeehenchen@gmail.com>
2018-11-21 12:03:55 +0800

    60%

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,34 @@
-// pages/index/index.js
+//index.js
+//获取应用实例
+
 Page({
-<<<<<<< HEAD
   data: {
-   array: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    hasUserInfo: false
   },
-  
-bindPickerChange: function (e) {
-    console.log('the chosen one!', e.detail.value)
-    this.setData({
-      index: e.detail.value
+
+  onLoad: function (options) {
+    let that = this;
+
+    wx.request({
+      url: 'https://cloud-suite.herokuapp.com/api/v1/jets',
+      method: 'GET',
+      success(res) {
+        console.log(res)
+        const jets = res.data
+        that.setData(
+          jets
+        )
+      }
     })
   },
-=======
-
-data: {},
->>>>>>> master
   //事件处理函数
   getUserInfo: function (e) {
     // update local storage with user info
     let user = wx.getStorageSync("currentUser")
     let currentUser = Object.assign(user, e.detail.userInfo)
-
+    this.setData({
+      hasUserInfo: true
+    })
     wx.setStorageSync("currentUser", currentUser)
   },
   jumpToPage: function () {
@@ -29,4 +37,3 @@ data: {},
     });
   }
 })
-
