c01685c012594d399d5b95ddf0619024dce89522
Benjamin Chen <yeehenchen@gmail.com>
2018-11-21 11:56:24 +0800

    create login function in index page, 50%

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,22 @@
 
 Page({
   data: {},
+  
+  onLoad: function (options) {
+    let that = this;
+
+    wx.request({
+      url: 'https://cloud-suite.herokuapp.com/api/v1/jets',
+      method: 'GET',
+      success(res) {
+        console.log(res)
+        const jets = res.data
+        that.setData(
+          jets
+        )
+      }
+    })
+  },
   //事件处理函数
   getUserInfo: function (e) {
     // update local storage with user info
